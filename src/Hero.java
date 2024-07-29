2ea45197d18de3ecd9dcd8d67ee0f78836f20109
Elmirbek Surkeev <elmirsurkeev@gmail.com>
2024-07-30 03:12:45 +0600

    create task first and second, but only dont while

---
+++ src/Hero.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+public class Hero {
+    private int hp;
+    private int defence;
+    private int atack;
+    private int weapon;
+    private int shield;
+
+    public Hero(int hp, int defence, int atack, int weapon, int shield){
+        this.hp = hp;
+        this.defence = defence;
+        this.atack = atack;
+        this.weapon = weapon;
+        this.shield = shield;
+    }
+
+    public int getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(int weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getAtack() {
+        return atack;
+    }
+    public void attackDragon(Hero dragon){
+        Random random = new Random();
+        int maybeDragonWantToAttack = random.nextInt(((1-1)+1)+1);
+        if (this.hp>0){
+            //В тз не указано когда не захочет бить Дракон, сделал вариант, что 50 на 50
+            //и также всегда бьет, чтобы код работал
+            if (maybeDragonWantToAttack>0 || 1>0){
+                int damageDragon = dragon.getAtack()+dragon.getWeapon()-this.shield;
+                this.hp -=damageDragon;
+                if(this.hp>0){
+                    System.out.println("Жизнь Героя после атаки Дракона в "+ damageDragon+ " осталась = "+ this.hp);
+                }
+            }
+            if(this.hp <= 0){
+                System.out.println("Дракон победил Героя");
+            }
+        }else {
+            System.out.println("Возникла ошибка в игре, нужно попробовать перезагрузить программу");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Random random = new Random();
+            int probablityAtackHero = random.nextInt(((5555-1)+1)+1);
+            if (probablityAtackHero >1){
+                Dragon dragon = new Dragon(2000, 120, 150, 0);
+                Hero hero = new Hero(100, 100, 125, 250, 150);
+                dragon.attackHero(hero);
+            }else{
+                System.out.println("Герой промазал, ждем ответного удара от Дракона");
+            }
     }
 }
